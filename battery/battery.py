47a7666cfe22c2d5875b3010bc34022d521794cc
Jenny Ren <jren@berkeley.edu>
2024-01-29 23:01:11 -0800

    attempting another commit

---
+++ battery/battery.py
@@ -0,0 +1,5 @@
+from abc import ABC
+
+def Battery(ABC):
+    def needs_service(self):
+        pass
---
+++ battery/nubbin_battery.py
@@ -0,0 +1,15 @@
+from battery.battery import Battery
+from utils import add_years_to_date
+
+
+class NubbinBattery(Battery):
+    def __init__(self, current_date, last_service_date):
+        self.current_date = current_date
+        self.last_service_date = last_service_date
+
+    def needs_service(self):
+        needs_service_date = self.last_service_date.replace(year=self.last_service_date.year + 4)
+        if needs_service_date < self.current_date:
+            return True
+        else:
+            return False
